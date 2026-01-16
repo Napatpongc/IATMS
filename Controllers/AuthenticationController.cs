1766b678004dde205d654f19c5f674b8c7c0c7f4
NutNapatong <napatpongct@gmail.com>
2026-01-16 09:53:44 +0700

    Add project files.

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using IATMS.Models.Payloads;
+using Microsoft.AspNetCore.Mvc;
+using System.DirectoryServices.Protocols;
+using System.Net;
+
+namespace IATMS.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("signin")]
+        public IActionResult Login([FromBody] Pay_signin input)
+        {
+
+            using var conn = new LdapConnection(new LdapDirectoryIdentifier("localhost", 389),new NetworkCredential($"cn={input.username},ou=users,dc=example,dc=org", input.password),AuthType.Basic);
+
+
+            try
+            {
+                conn.Bind();
+                return Ok(new { success = true, message = "LDAP bind success" });
+            }
+            catch (LdapException)
+            {
+                return Unauthorized(new { success = false, message = "Invalid username or password" });
+            }
+        }
+    }
+}
---
+++ Models/Payloads/Authentication.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IATMS.Models.Payloads
+{
+    public class Pay_signin 
+    {
+        [Required]
+        [MaxLength(100,ErrorMessage ="Maximum length")]
+        public string username { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Maximum length")]
+        public string password { get; set; }
+    }
+}
